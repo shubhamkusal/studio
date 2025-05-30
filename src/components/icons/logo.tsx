cb37593d2c7ce63cea09b12e6b1a9f3e662dd644
Shubham Kusal <shubhamkusal19@gmail.com>
2025-05-30 19:10:39 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Logo className="h-8 w-auto">
    
    with the text content:
    
        TRACKERLY
    
    Relevant files:
    
        - https://6000-firebase-studio-1748629593835.cluster-w5vd22whf5gmav2vgkomwtc4go.cloudworkstations.dev/_next/static/chunks/node_modules_d4db0bf7._.js
        - https://6000-firebase-studio-1748629593835.cluster-w5vd22whf5gmav2vgkomwtc4go.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    FIX IT

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -2,7 +2,26 @@ import type { SVGProps } from 'react';
 
 const Logo = (props: SVGProps<SVGSVGElement>) => (
   <svg width="150" height="36" viewBox="0 0 160 36" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <text x="0" y="27" fontFamily="Inter, sans-serif" fontSize="28" fontWeight="bold" fill="hsl(var(--foreground))">
+    <defs>
+      <filter id="logo-glow" x="-50%" y="-50%" width="200%" height="200%">
+        <feGaussianBlur in="SourceAlpha" stdDeviation="1.5" result="blur" />
+        <feFlood floodColor="hsl(var(--primary))" floodOpacity="0.7" result="color" />
+        <feComposite in="color" in2="blur" operator="in" result="coloredBlur" />
+        <feMerge>
+          <feMergeNode in="coloredBlur"/>
+          <feMergeNode in="SourceGraphic"/>
+        </feMerge>
+      </filter>
+    </defs>
+    <text
+      x="0"
+      y="27"
+      fontFamily="Inter, sans-serif"
+      fontSize="28"
+      fontWeight="bold"
+      fill="hsl(var(--foreground))"
+      filter="url(#logo-glow)"
+    >
       TRACKERLY
     </text>
   </svg>
